6677b75e3a97a17cea15160ee53465b57f136c97
Jimin0304 <pjws0304@naver.com>
2025-06-29 19:15:43 +0900

    [⚙️ CHORE] 기존 하나원카에 구현된 본인인증 및 회원가입을 kftc로 옮김

--- src/main/java/com/onecar/common/exception/ErrorCode.java
+++ src/main/java/com/onecar/common/exception/ErrorCode.java
@@ -17,21 +17,6 @@ public enum ErrorCode {
     DUPLICATED_USERNAME(409, "M_002", "이미 존재하는 아이디입니다."),
     INVALID_PASSWORD(401, "M_003", "비밀번호가 올바르지 않습니다."),
     INVALID_TOKEN(401, "M_004", "토큰이 만료되었습니다."),
-
-    // 카드 관련 에러 코드
-    INVALID_AUTHORIZATION(401, "K_001", "Authorization 헤더가 올바르지 않습니다."),
-    INVALID_SCOPE(400, "K_002", "서비스구분이 올바르지 않습니다. (cardinfo만 허용)"),
-    INVALID_AGREEMENT(400, "K_003", "제3자정보제공동의가 필요합니다."),
-    ALREADY_REGISTERED_USER(400, "A0324", "기등록된 조회서비스용 사용자 서비스"),
-    USER_WITHDRAWAL_IN_PROGRESS(400, "A0019", "사용자탈퇴 처리중인 서비스"),
-    USER_NOT_FOUND(400, "555", "해당 사용자 없음"),
-    USER_REGISTRATION_ERROR(400, "464", "사용자 등록정보 이상"),
-    
-    // 사용자 등록 관련 에러 코드
-    DUPLICATED_PHONE_NUMBER(409, "U_001", "이미 등록된 휴대폰번호입니다."),
-    DUPLICATED_SOCIAL_SECURITY_NUMBER(409, "U_002", "이미 등록된 주민등록번호입니다."),
-    PHONE_VERIFICATION_REQUIRED(400, "U_003", "휴대폰 인증이 필요합니다."),
-    INVALID_SOCIAL_SECURITY_NUMBER(400, "U_004", "유효하지 않은 주민등록번호입니다.")
     ;
 
     private final int status;
